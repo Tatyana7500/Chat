b3bf4dc3c4a8f9aae0fd03abf115df98068c374f
marselenka <shkodina-le@yandex.ru>
2019-09-11 08:11:09 +0300

    CHAT-25-websoket-api-connection

--- src/server.js
+++ src/server.js
@@ -1,6 +1,28 @@
 const express = require('express');
-
 const app = express();
 
+const server = app.listen(3000);
 app.use(express.static('public'));
-app.listen(3000);
+console.log('my server is running');
+
+const socket =require('socket.io');
+const io=socket(server);
+
+io.sockets.on('connection', newConnection);
+
+function newConnection(socket) {
+    console.log(`new connection: `+socket.id);
+    socket.on('chat message', message);
+
+    function message(msg) {
+        if(msg.type==="newMessage"){
+            io.sockets.emit('chat message', msg + 'echo');
+        }
+    }
+    socket.on('disconnect', disconnected);
+    function disconnected() {
+        console.log('client has disconnected');
+
+    }
+}
+
